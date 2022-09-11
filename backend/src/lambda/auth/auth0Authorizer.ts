0524f0e2a8a02aecc383c1687578d4bcbf2a4e22
fedirjh <fedirjh@gmail.com>
2022-09-11 20:15:37 +0100

    deploy serverless ci/cd

--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -59,6 +59,7 @@ async function verifyToken(authHeader: string): Promise<JwtPayload> {
   const jwt: Jwt = decode(token, { complete: true }) as Jwt
 
   if (!jwt || !jwt.header || !jwt.header.kid || !jwt.header.alg || jwt.header.alg !== 'RS256' ) {
+    logger.error('Invalid JWT token')
     throw new Error('invalid token');
   }
 
@@ -68,15 +69,27 @@ async function verifyToken(authHeader: string): Promise<JwtPayload> {
   // You should implement it similarly to how it was implemented for the exercise for the lesson 5
   // You can read more about how to do this here: https://auth0.com/blog/navigating-rs256-and-jwks/
 
-  // Get keys from Auth0
-  const jwks = await Axios.get(jwksUrl).then(res => res.data);
+  try {
+    // Get keys from Auth0
+    const jwks = await Axios.get(jwksUrl).then(res => res.data)
 
-  // Get Certificate
-  const cert:string = getSingingKey(jwks, keyId);
+    // Get Certificate
+    const cert = getSingingKey(jwks, keyId);
 
-  if(!cert) throw new Error('Invalid token')
+    if(!cert) {
+      logger.error('Invalid Certificate', { cert })
+      throw new Error('Invalid token')
+    }
 
-  return verify(token, cert, { algorithms: ['RS256'] }) as JwtPayload
+    return verify(token, cert, { algorithms: ['RS256'] }) as JwtPayload
+
+  } catch (e) {
+
+    logger.error('Error fetching keys', { e })
+    throw new Error('Invalid token')
+
+  }
+
 }
 
 function getToken(authHeader: string): string {
@@ -89,25 +102,19 @@ function getToken(authHeader: string): string {
   return split[1]
 }
 
-function getSingingKey(jwks: any, kid: string): string {
+function getSingingKey(jwks: any, kid: string): any {
   const signingKeys = jwks.keys.filter(key => key.use === 'sig' // JWK property `use` determines the JWK is for signing
     && key.kty === 'RSA' // We are only supporting RSA (RS256)
     && key.kid           // The `kid` must be present to be useful for later
     && ((key.x5c && key.x5c.length) || (key.n && key.e)) // Has useful public keys
   ).map(key => {
-    return certToPEM(key.x5c[0]);
+    return { kid: key.kid, nbf: key.nbf, publicKey: certToPEM(key.x5c[0]) };
   });
   return signingKeys.find(key => key.kid === kid);
 }
 
 function certToPEM(cert): string {
-  cert = cert.match(/.{1,64}/g).join(`
-
-`);
-  cert = `-----BEGIN CERTIFICATE-----
-
-${cert}
-
------END CERTIFICATE-----\n`;
+  cert = cert.match(/.{1,64}/g).join('\n');
+  cert = `-----BEGIN CERTIFICATE-----\n${cert}\n-----END CERTIFICATE-----\n`;
   return cert;
 }
